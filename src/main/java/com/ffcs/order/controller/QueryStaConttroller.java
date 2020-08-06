f41066556e532320707993284d032d425b348f34
zhangyong <1430379325@qq.com>
2020-08-06 10:24:08 +0800

    修改了数据库配置文件

--- src/main/java/com/ffcs/order/controller/QueryStaConttroller.java
+++ src/main/java/com/ffcs/order/controller/QueryStaConttroller.java
@@ -6,18 +6,23 @@ import com.ffcs.order.entity.OrderStatistics;
 import com.ffcs.order.entity.Statistics;
 import com.ffcs.order.mapper.QueryStaMapper;
 import com.ffcs.order.tools.JWTtool;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Api(tags = "统计数据管理")
+@RestController
 public class QueryStaConttroller {
     @Autowired
     private QueryStaMapper qsm;
     @Autowired
     private JWTtool jwTtool;
+    @ApiOperation(value = "查询商品类别统计数据接口，入参商品id", notes = "")
     @RequestMapping(path = "/QueryStaById", method = RequestMethod.POST)
     public String QueryStaById(@RequestParam String commodityId) {
         JSONObject json=new JSONObject();
@@ -25,7 +30,7 @@ public class QueryStaConttroller {
         String gson= json.toJSONString(us);
         return gson;
     }
-
+    @ApiOperation(value = "按天查询统计数据接口", notes = "")
     @RequestMapping(path = "/QuerySta", method = RequestMethod.POST)
     public String QuerySta(@RequestParam String updateTime) {
         JSONObject json=new JSONObject();
--- src/main/java/com/ffcs/order/controller/RegisterController.java
+++ src/main/java/com/ffcs/order/controller/RegisterController.java
@@ -2,20 +2,24 @@ package com.ffcs.order.controller;
 
 import com.ffcs.order.entity.User;
 import com.ffcs.order.mapper.RegisterMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-@Controller
+@Api(tags = "注册")
+@RestController
 public class RegisterController {
     @Autowired
     private RegisterMapper re;
+    @ApiOperation(value = "注册接口", notes = "")
     @RequestMapping(path = "/register", method = RequestMethod.POST)
     public String register(@RequestParam(required=false) String userId,
                            @RequestParam(required=false) String userName,
